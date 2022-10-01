cae91289fe2523aeacd9814127489844c647699d
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-10-01 22:26:43 +0200

    postsReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import { Counter } from './features/counter/Counter';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Counter />
+      <hr />
+      <AddPostForm />
+      <PostsList />
+    </>
   );
 }
 export default App;
